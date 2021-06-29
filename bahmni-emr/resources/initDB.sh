f86e51a217143ae07ff088b67fb9f5005f32a76e
Hiren Thacker <hithacker@gmail.com>
2021-06-29 09:42:20 +0530

    Added script to run bahmni-emr postInstall

--- bahmni-emr/resources/initDB.sh
+++ bahmni-emr/resources/initDB.sh
@@ -7,14 +7,14 @@ if [ -f /etc/bahmni-installer/bahmni.conf ]; then
 . /etc/bahmni-installer/bahmni.conf
 fi
 
-RESULT=`mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD --skip-column-names -e "SHOW DATABASES LIKE 'openmrs'"`
-if [ "$RESULT" != "openmrs" ] ; then
-  mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE openmrs;"
+RESULT=`mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD --skip-column-names -e "SHOW DATABASES LIKE '$OPENMRS_DB_NAME'"`
+if [ "$RESULT" != "$OPENMRS_DB_NAME" ] ; then
+  mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $OPENMRS_DB_NAME;"
   if [ "${IMPLEMENTATION_NAME:-default}" = "default" ]; then
-    echo "openmrs database not found... Restoring a base dump suitable to work with default config"
-    mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD openmrs < scripts/openmrs_demo_dump.sql
+    echo "$OPENMRS_DB_NAME database not found... Restoring a base dump suitable to work with default config"
+    mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD $OPENMRS_DB_NAME < scripts/openmrs_demo_dump.sql
   else
-    echo "clean openmrs database will be created with no demo data"
+    echo "clean $OPENMRS_DB_NAME database will be created with no demo data"
     mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD  < scripts/openmrs_clean_dump.sql
   fi
 fi
