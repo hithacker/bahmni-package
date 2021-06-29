a2f7bd5b6ad7536ab2ecc48d338873b75b670a46
Hiren Thacker <hithacker@gmail.com>
2021-06-29 09:42:20 +0530

    Added script to run bahmni-emr postInstall

---
+++ bahmni-emr/resources/runDBFunctions.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e -x
+
+if [ -f /etc/bahmni-installer/bahmni.conf ]; then
+. /etc/bahmni-installer/bahmni.conf
+fi
+
+. /etc/openmrs/openmrs.conf
+. /etc/openmrs/bahmni-emr.conf
+
+run_migrations() {
+    echo "Running openmrs liquibase-core-data.xml and liquibase-update-to-latest.xml"
+    /opt/openmrs/etc/run-liquibase.sh liquibase-core-data.xml
+    /opt/openmrs/etc/run-liquibase.sh liquibase-update-to-latest.xml
+}
+
+./initDB.sh
+run_migrations
